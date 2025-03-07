bec639276a98f653d5ad03bd4e2cd331005792a1
mehdi <mreko95@protonmail.com>
2025-03-07 11:03:44 +0330

    addmotionInNavbar

--- src/app/components/NavbarItems.tsx
+++ src/app/components/NavbarItems.tsx
@@ -2,9 +2,8 @@
 import Link from "next/link";
 import { Search, UserRound, ShoppingCart, Menu } from "lucide-react";
 import { Input } from "@/components/ui/input";
+import {motion} from "framer-motion"
 import { useState } from "react";
-// import { motion } from "framer-motion"; // وارد کردن Framer Motion
-
 import { Button } from "@/components/ui/button";
 
 export default function NavbarItems() {
@@ -17,41 +16,24 @@ export default function NavbarItems() {
     }
   };
 
-   // تنظیمات انیمیشن
-  //  const menuVariants = {
-  //   open: {
-  //     y: 0,
-  //     opacity: 1,
-  //     transition: { duration: 0.3, ease: "easeInOut" },
-  //   },
-  //   closed: {
-  //     y: "-100%",
-  //     opacity: 0,
-  //     transition: { duration: 0.3, ease: "easeInOut" },
-  //   },
-  // };
-
-
   return (
     <>
       <nav className="container mx-auto flex justify-center w-full gap-6 py-2 ">
+    <motion.div
+    initial={{opacity:0,y:-50}}
+    animate={{opacity:1,y:0}}
+    transition={{duration:0.5}}
+    >
         {/* menu */}
         <div className="bg-blue-500/30 backdrop-sepia  flex flex-col w-full md:w-auto md:flex-row gap-4 md:gap-7 p-2 rounded-2xl ">
           <div className="flex items-center md:hidden ">
-            <div onClick={() => setIsOpen(!isOpen)} >
+            <div onClick={() => setIsOpen(!isOpen)}>
               <Menu />
             </div>
 
-            {/* <motion.div
-          className="flex flex-col md:flex-row gap-4 md:gap-7 w-full absolute md:static top-12 left-0 md:top-auto md:left-auto bg-blue-500/30 md:bg-transparent p-2 md:p-0 rounded-2xl md:rounded-none"
-          initial={false}
-          animate={isOpen ? "open" : "closed"}
-          variants={menuVariants}
-          >
-          </motion.div> */}
             <Link
               href="/"
-              className="ml-20 font-serif font-bold text-xl hover:bg-gray-500 p-2 rounded-sm duration-500 ease-in-out text-center md:text-left "
+              className="ml-12 font-serif font-bold text-xl hover:bg-gray-500 p-2 rounded-sm duration-500 ease-in-out text-center md:text-left "
             >
               TechZone
             </Link>
@@ -154,7 +136,10 @@ export default function NavbarItems() {
             </div>
           </div>
         </div>
+        </motion.div>
       </nav>
     </>
   );
 }
+
+                  
