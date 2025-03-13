b2d7056d4b0e539531ef3c99900f688b8a506647
mehdi <mreko95@protonmail.com>
2025-03-13 18:06:43 +0330

    completeProductPage

---
+++ src/app/Cart/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Cart() {
+  return (
+    <div className='m-5'>
+      Cart Page
+    </div>
+  )
+}
--- src/app/components/ProductModal.tsx
+++ src/app/components/ProductModal.tsx
@@ -6,12 +6,17 @@ import Image from "next/image";
 import { X, ShoppingBag } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { motion } from "framer-motion";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";//remember for use next13+ otherwise next/Rouetr in next13-
 
 const modalVariants = {
   hidden: { opacity: 0, scale: 0.9 },
-  visible: { opacity: 1, scale: 1, transition: { duration: 0.3, ease: "easeInOut" } },
+  visible: {
+    opacity: 1,
+    scale: 1,
+    transition: { duration: 0.3, ease: "easeInOut" },
+  },
 };
-
 
 function ProductModal() {
   const dispatch = useDispatch<AppDispatch>();
@@ -19,29 +24,41 @@ function ProductModal() {
     (state: RootState) => state.productModal
   );
 
+  const [isAdd, setIsAdd] = useState(false);
+  const router=useRouter()
+
+  useEffect(() => {
+    setIsAdd(false);
+  }, [isOpen]);
+
   if (!isOpen || !selectedProduct) return null;
 
   const handleAddtocart = () => {
-    dispatch(addToCart(selectedProduct));
+    if(!isAdd){
+      dispatch(addToCart(selectedProduct));
+      setIsAdd(true);
+    }else{
+      router.push("/Cart")
+    }
+
   };
 
   return (
     <motion.div
-    className="fixed inset-0 z-10 bg-gray-800/80 flex justify-center items-center"
-    initial="hidden"
-    animate={isOpen ? "visible" : "hidden"}
-    onClick={() => dispatch(closeModal())}
-  >
-    <motion.div
-      className="grid md:grid-cols-2 bg-white p-5 rounded-lg w-full max-w-7xl h-screen md:h-3/4 mx-4"
-      variants={modalVariants}
+      className="fixed inset-0 z-10 bg-gray-800/80 flex justify-center items-center"
       initial="hidden"
-      animate="visible"
-      exit="hidden"
-      onClick={(e) => e.stopPropagation()}
+      animate={isOpen ? "visible" : "hidden"}
+      onClick={() => dispatch(closeModal())}
     >
-
-        <div>
+      <motion.div
+        className="grid md:grid-cols-2 bg-white p-5 rounded-lg w-full max-w-7xl h-screen md:h-3/4 mx-4"
+        variants={modalVariants}
+        initial="hidden"
+        animate="visible"
+        exit="hidden"
+        onClick={(e) => e.stopPropagation()}
+      >
+        <div className="-mb-24 md:flex md:justify-center md:items-center">
           <Image
             src={selectedProduct.image}
             alt="product image"
@@ -51,20 +68,39 @@ function ProductModal() {
           />
         </div>
         <div className="flex flex-col justify-center">
-          <h2 className="text-xl font-bold mb-2">{selectedProduct.title}</h2>
-          <p>{selectedProduct.price}:</p>
-          <p>{selectedProduct.brand}:</p>
-          <p>{selectedProduct.model}:</p>
-          <p>{selectedProduct.color}:</p>
-          <h3 className="hidden md:flex">About this product:</h3>
+          <h2 className="text-xl font-bold mb-2">
+            {selectedProduct.title.slice(0, 100)}
+          </h2>
+          <p className="text-3xl mb-2 font-bold before:content-['$'] before:text-green-500">
+            {selectedProduct.price}
+          </p>
+          <p>
+            <span className="font-bold">Brand:</span> {selectedProduct.brand}
+          </p>
+          <p>
+            <span className="font-bold">Moddel:</span> {selectedProduct.model}
+          </p>
+          <p>
+            <span className="font-bold">Color:</span> {selectedProduct.color}
+          </p>
+          <h3 className="hidden md:flex md:text-xl font-bold mt-2">
+            About this product:
+          </h3>
           <p className="hidden md:flex text-gray-700 mb-2">
             {selectedProduct.description}
           </p>
           <div className="flex gap-2">
-            <Button className="mt-2 rounded-2xl" onClick={handleAddtocart}>
+            <Button
+              className={`mt-2 rounded-2xl ${
+                isAdd ? "bg-red-600 hover:bg-red-400" : ""
+              }`}
+              onClick={handleAddtocart}
+            >
               <ShoppingBag />
-              Add to Cart
+              {isAdd ? "Go To Cart" : "Add To Cart"}
             </Button>
+            
+            {/* delete button */}
             <button
               className="absolute top-0 right-4 md:top-32 md:right-[320px] bg-white text-red-600 px-4 py-2 rounded hover:bg-red-400 hover:text-white"
               onClick={() => dispatch(closeModal())}
@@ -73,7 +109,7 @@ function ProductModal() {
             </button>
           </div>
         </div>
-    </motion.div>
+      </motion.div>
     </motion.div>
   );
 }
