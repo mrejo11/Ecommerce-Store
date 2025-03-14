2f2c7def423664b3d3b5fd2fda824b04d5fbe914
mehdi <mreko95@protonmail.com>
2025-03-14 18:05:07 +0330

    bugfix-MobileResponsive

--- src/app/components/ProductModal.tsx
+++ src/app/components/ProductModal.tsx
@@ -44,76 +44,72 @@ function ProductModal() {
   };
 
   return (
-    <motion.div
-      className="fixed inset-0 z-10 bg-gray-800/80 flex justify-center items-center"
-      initial="hidden"
-      animate={isOpen ? "visible" : "hidden"}
+<motion.div
+  className="fixed inset-0 z-10 bg-gray-800/80 flex justify-center items-center"
+  initial="hidden"
+  animate={isOpen ? "visible" : "hidden"}
+  onClick={() => dispatch(closeModal())}
+>
+  <motion.div
+    className="relative grid md:grid-cols-2 bg-white p-5 rounded-lg w-full max-w-[80%] h-screen md:h-[80vh] overflow-auto"
+    variants={modalVariants}
+    initial="hidden"
+    animate="visible"
+    exit="hidden"
+    onClick={(e) => e.stopPropagation()}
+  >
+    {/* close button */}
+    <button
+      className="absolute top-4 right-4 bg-white text-red-600 px-3 py-1 rounded hover:bg-red-400 hover:text-white"
       onClick={() => dispatch(closeModal())}
     >
-     
-      <motion.div
-        className="grid md:grid-cols-2 bg-white p-5 rounded-lg w-full max-w-10/12 h-screen md:h-[90vh]"
-        variants={modalVariants}
-        initial="hidden"
-        animate="visible"
-        exit="hidden"
-        onClick={(e) => e.stopPropagation()}
-      >       
-        <div className=" md:flex items-center ">
-          <Image
-            src={selectedProduct.image}
-            alt="product image"
-            width={500}
-            height={300}
-           
-          />
-        </div>
-        <div className=" flex flex-col justify-center">
-          <h2 className="text-xl font-bold mb-2">
-            {selectedProduct.title.slice(0, 100)}
-          </h2>
-          <p className="text-3xl mb-2 font-bold before:content-['$'] before:text-green-500">
-            {selectedProduct.price}
-          </p>
-          <p>
-            <span className="font-bold">Brand:</span> {selectedProduct.brand}
-          </p>
-          <p>
-            <span className="font-bold">Moddel:</span> {selectedProduct.model}
-          </p>
-          <p>
-            <span className="font-bold">Color:</span> {selectedProduct.color}
-          </p>
-          <h3 className="hidden md:flex md:text-xl font-bold mt-2">
-            About this product:
-          </h3>
-          <p className="hidden md:flex text-gray-700 mb-2 md:truncat md:w-150">
-            {selectedProduct.description.slice(0,550)}
-          </p>
-          <div className="flex gap-2">
+      <X />
+    </button>
 
-            <Button
-              className={`mt-2 rounded-2xl ${
-                isAdd ? "bg-red-600 hover:bg-red-400" : ""
-              }`}
-              onClick={handleAddtocart}
-            >
-              <ShoppingBag />
-              {isAdd ? "Go To Cart" : "Add To Cart"}
-            </Button>
-            
-            {/* delete button */}
-            <button
-              className=" md:-translate-y-[480px] md:translate-x-[440px] bg-white text-red-600 px-4 py-2 rounded hover:bg-red-400 hover:text-white"
-              onClick={() => dispatch(closeModal())}
-            >
-              <X />
-            </button>
+    <div className="flex justify-center items-center p-4">
+      <Image
+        src={selectedProduct.image}
+        alt="product image"
+        width={500}
+        height={300}
+      />
+    </div>
 
-          </div>
-        </div>
-      </motion.div>
-    </motion.div>
+    <div className="flex flex-col justify-center p-4">
+      <h2 className="text-xl font-bold mb-2">
+        {selectedProduct.title.slice(0, 100)}
+      </h2>
+      <p className="text-3xl mb-2 font-bold before:content-['$'] before:text-green-500">
+        {selectedProduct.price}
+      </p>
+      <p>
+        <span className="font-bold">Brand:</span> {selectedProduct.brand}
+      </p>
+      <p>
+        <span className="font-bold">Model:</span> {selectedProduct.model}
+      </p>
+      <p>
+        <span className="font-bold">Color:</span> {selectedProduct.color}
+      </p>
+      <h3 className="text-xl font-bold mt-2">
+        About this product:
+      </h3>
+      <p className="text-gray-700 mb-2">
+        {selectedProduct.description}
+      </p>
+      <div className="flex gap-2">
+        <Button
+          className={`mt-2 rounded-2xl ${isAdd ? "bg-red-600 hover:bg-red-400" : ""}`}
+          onClick={handleAddtocart}
+        >
+          <ShoppingBag />
+          {isAdd ? "Go To Cart" : "Add To Cart"}
+        </Button>
+      </div>
+    </div>
+  </motion.div>
+</motion.div>
+
   );
 }
 
