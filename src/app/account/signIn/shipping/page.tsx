dfd1de3a01a6f48f65d8d1e6327ea39f8e169c6d
mehdi <mreko95@protonmail.com>
2025-03-29 00:25:06 +0330

    addReactHookForm

--- src/app/account/signIn/shipping/page.tsx
+++ src/app/account/signIn/shipping/page.tsx
@@ -11,15 +11,38 @@ import {
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { useState, useRef, useEffect } from "react";
-import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useForm, SubmitHandler } from "react-hook-form";
 
+type Inputs = {
+  firstName: string;
+  lastName: string;
+  email: string;
+  postId:number;
+  city:string;
+  address:string;
+  phone:number
+};
 
 export default function ShippingPage() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>();
+  // const onError: SubmitErrorHandler<FormValues> = (errors) => console.log(errors);
+
   const [isTextarea, setIsTextarea] = useState(false);
   const [address, setAddress] = useState("");
   const textareaRef = useRef<HTMLTextAreaElement>(null);
+  const router = useRouter();
   const handleClick = () => {
     setIsTextarea(true);
+  };
+
+  const handleOnSubmit: SubmitHandler<Inputs> = (data) => {
+    console.log(data);
+    router.push("/payment");
   };
 
   const handleAddressChange = (
@@ -27,7 +50,6 @@ export default function ShippingPage() {
   ) => {
     setAddress(e.target.value);
   };
-
 
   // check aya click khareg as safeh shode
   useEffect(() => {
@@ -52,100 +74,153 @@ export default function ShippingPage() {
   }, [isTextarea]);
 
   return (
-    <div className="h-screen flex items-center justify-center">
-      <div className="grid grid-cols-2 gap-4 w-full max-w-2xl h-auto border p-4 rounded-2xl shadow">
-        <div className="col-span-2">
-          <h1 className="text-2xl font-bold font-mono">SHIPPING ADDRESS</h1>
-          <p className="text-gray-700 font-mono">
-            Please enter your shipping address in the form below.
-          </p>
-        </div>
+    <form onSubmit={handleSubmit(handleOnSubmit)}>
+      <div className="h-screen flex items-center justify-center">
+        <div className="grid grid-cols-2 gap-4 w-full max-w-2xl h-auto border p-4 rounded-2xl shadow">
+          <div className="col-span-2">
+            <h1 className="text-2xl font-bold font-mono">SHIPPING ADDRESS</h1>
+            <p className="text-gray-700 font-mono">
+              Please enter your shipping address in the form below.
+            </p>
+          </div>
+          <div>
+            <Select>
+              <SelectTrigger className="w-full">
+                <SelectValue placeholder="Salutation *" />
+              </SelectTrigger>
+              <SelectContent className="font-mono text-stone-500">
+                <SelectGroup>
+                  <SelectItem value="mr">Mr.</SelectItem>
+                  <SelectItem value="mrs">Mrs.</SelectItem>
+                  <SelectItem value="ms">Ms.</SelectItem>
+                  <SelectItem value="mx">Mx</SelectItem>
+                  <SelectItem value="none">No specification</SelectItem>
+                </SelectGroup>
+              </SelectContent>
+            </Select>
+          </div>
 
-        <div>
-          <Select>
-            <SelectTrigger className="w-full">
-              <SelectValue placeholder="Salutation *" />
-            </SelectTrigger>
-            <SelectContent className="font-mono text-stone-500">
-              <SelectGroup>
-                <SelectItem value="mr">Mr.</SelectItem>
-                <SelectItem value="mrs">Mrs.</SelectItem>
-                <SelectItem value="ms">Ms.</SelectItem>
-                <SelectItem value="mx">Mx</SelectItem>
-                <SelectItem value="none">No specification</SelectItem>
-              </SelectGroup>
-            </SelectContent>
-          </Select>
-        </div>
-
-        <div>
-          <Input id="name" type="text" placeholder="Name" className="w-full" />
-        </div>
-        <div>
-          <Input
-            id="lastName"
-            type="text"
-            placeholder="Last Name"
-            className="w-full"
-          />
-        </div>
-        <div>
-          <Input
-            id="email"
-            type="email"
-            placeholder="Email"
-            className="w-full"
-          />
-        </div>
-        <div>
-          <Input
-            id="postcode"
-            type="number"
-            placeholder="Post code"
-            className="w-full"
-          />
-        </div>
-        <div>
-          <Input id="city" type="text" placeholder="City" className="w-full" />
-        </div>
-
-        <div>
-          {isTextarea ? (
-            <textarea
-              ref={textareaRef}
-              id="address"
-              placeholder="Address"
-              value={address}
-              onChange={handleAddressChange}
-              className="w-full h-24 p-2 border rounded-md"
-            />
-          ) : (
+          <div>
             <Input
-              id="address"
-              type="text"
-              placeholder="Address"
-              value={address}
+              {...register("firstName",{
+                required:"firstname is requird",
+                minLength:2,
+              })}
+              placeholder="Name"
               className="w-full"
-              onChange={handleAddressChange}
-              onClick={handleClick}
             />
-          )}
-        </div>
-        <div>
-          <Input
-            id="phone"
-            type="number"
-            placeholder="Phone"
-            className="w-full"
-          />
-        </div>
+            {errors.firstName && (
+               <span className="text-red-600 font-mono">
+               {errors.firstName.message}
+             </span>
+            )}
+          </div>
+          <div>
+            <Input
+              {...register("lastName", {
+                required: "lastname is required",
+                minLength: 2,
+              })}
+              placeholder="Lastname"
+              className="w-full"
+            />
+            {errors.lastName && (
+              <span className="text-red-600 font-mono">
+                {errors.lastName.message}
+              </span>
+            )}
+          </div>
+          <div>
+            <Input
+            {...register("email",{
+              required: "email is required",
+                minLength: 2,
+            })}
+              placeholder="Email"
+              className="w-full"
+            />
+              {errors.email && (
+              <span className="text-red-600 font-mono">
+                {errors.email.message}
+              </span>
+            )}
+          </div>
+          <div>
+            <Input
+              {...register("postId",{
+                required: "PostId is required",
+                  minLength: 2,
+              })}
+              placeholder="Post code"
+              className="w-full"
+            />
+              {errors.postId && (
+              <span className="text-red-600 font-mono">
+                {errors.postId.message}
+              </span>
+            )}
+          </div>
+          <div>
+            <Input
+               {...register("city",{
+                required: "ciry is required",
+                  minLength: 2,
+              })}
+              placeholder="City"
+              className="w-full"
+            />
+            {errors.city && (
+              <span className="text-red-600 font-mono">
+                {errors.city.message}
+              </span>
+            )}
+          </div>
 
-        <div className="col-span-2">
-          <Link href={"/payment"}>
-            <Button className="w-full mt-2">Proceed</Button>
-          </Link>
+          <div>
+            {isTextarea ? (
+              <textarea
+                ref={textareaRef}
+                id="address"
+                placeholder="Address"
+                value={address}
+                onChange={handleAddressChange}
+                className="w-full h-24 p-2 border rounded-md"
+              />
+            ) : (
+              <Input
+                id="address"
+                type="text"
+                placeholder="Address"
+                value={address}
+                className="w-full"
+                onChange={handleAddressChange}
+                onClick={handleClick}
+              />
+            )}
+          </div>
+          <div>
+            <Input
+              {...register("phone",{
+                required: "phone is required",
+                  minLength: 2,
+              })}
+              placeholder="Phone"
+              className="w-full"
+            />
+             {errors.phone && (
+              <span className="text-red-600 font-mono">
+                {errors.phone.message}
+              </span>
+            )}
+          </div>
+
+          <div className="col-span-2">
+            <Button type="submit" className="w-full mt-2">
+              Proceed
+            </Button>
+          </div>
         </div>
       </div>
-    </div>
+    </form>
   );
 }
